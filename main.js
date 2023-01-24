58960cc13dfef233bf1b23ec629a7c75e4ad1bd0
daniel <persensus@yahoo.com.ar>
2023-01-24 19:37:35 -0300

    add function button toggle shading, function color random

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const input = document.getElementsByTagName("input")[2]
 const label_input = document.getElementById("label_input")
 const blocks = document.getElementById("blocks")
+const front_color= document.getElementById("front_color")
+const backgroundColor = document.getElementById("background_color")
+let shading = false
 
 function createContainer(){
     const container = document.createElement("div")
@@ -8,6 +11,22 @@ function createContainer(){
     blocks.insertBefore(container, label_input.nextSibling)
     return container
 }
+
+function shadowToggle(){
+    return shading ? shading = false : shading = true
+}
+
+function sha(bolean){
+    const btnShadow = document.getElementById("btnShadow")
+    if (bolean){
+        btnShadow.style.backgroundColor = "black"
+        btnShadow.style.color = "white"}
+    else{
+        btnShadow.style.backgroundColor = "blueviolet"
+        btnShadow.style.color = "black"}
+}
+
+
 
 function createDivs(container,numbGrid = 16){
     label_input.innerText = `${numbGrid}X${numbGrid}`
@@ -17,8 +36,9 @@ function createDivs(container,numbGrid = 16){
         container.appendChild(divRow)
         for (let j = 0; j < numbGrid; j++) {    
             const divCell = document.createElement("div")
+            divCell.style.backgroundColor = backgroundColor.value
             divCell.onmouseover = function() { 
-                divCell.style.backgroundColor = "yellow"
+                divCell.style.backgroundColor = front_color.value
                 divCell.style.transition = "0.5s"
             };
             divCell.classList.add("divCell")
@@ -32,11 +52,39 @@ function reset(){
     createDivs(createContainer(),input.value)
 }
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function randomColor(color){
+    changeBackGrounColor(color)
+    changeFrontColor()
+}
+
+function changeBackGrounColor(color = backgroundColor.value){
+    let divCell = document.getElementsByClassName("divCell")
+    for (let i = 0; i < divCell.length; i++) {  
+        divCell[i].style.backgroundColor = color;    
+    }
+    backgroundColor.value = color
+}
+
+function changeFrontColor(){
+    front_color.value = getRandomColor()
+}
+
 input.addEventListener("keypress", ()=>{
     if (event.key === "Enter") {  
         document.getElementsByClassName("container")[0].remove()  
         createDivs(createContainer(), input.value)
     }  
 })
+
+backgroundColor.addEventListener("input",()=> changeBackGrounColor())
 
 createDivs(createContainer(),)
